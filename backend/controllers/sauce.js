11b72796e41cc792d3620c2193ff69486d3a60e2
Charles Helliet <charles.helliet@elamp.fr>
2021-05-03 17:58:00 +0200

    updateOne

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -35,11 +35,11 @@ exports.updateSauce = (req, res, next) => {
       {
         ...JSON.parse(req.body.sauce),
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-      } : { ...req.body };
+      } : { ...JSON.parse(req.body) };
     Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
       .then(() => res.status(200).json({ message: 'Sauce mise à jour !'}))
       .catch(error => res.status(400).json({ error }));
-  };
+  }; 
 
 exports.deleteSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
